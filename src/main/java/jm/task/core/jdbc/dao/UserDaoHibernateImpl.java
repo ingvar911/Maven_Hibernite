6cc52b7844312a85eb8274a7989ea486d1e1f87a
ingvar911 <ingvarkazan@gmail.com>
2023-05-31 14:49:03 +0300

    Добавил try с ресурсами в UserDaoHibernateImpl, убрал try из Util

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -6,6 +6,7 @@ import org.hibernate.Session;
 import org.hibernate.Transaction;
 
 import javax.persistence.Query;
+import java.sql.SQLException;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
@@ -15,57 +16,57 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
-        Session session = Util.getSessionFactory().openSession();
-        Transaction transaction = session.beginTransaction();
-        String sql = "CREATE TABLE IF NOT EXISTS User (ID INT AUTO_INCREMENT PRIMARY KEY, Name TEXT, LastName TEXT, Age TINYINT)";
-        Query query = session.createSQLQuery(sql).addEntity(User.class);
-        query.executeUpdate();
-        session.close();
+        try (Session session = Util.getSessionFactory().openSession();) {
+            Transaction transaction = session.beginTransaction();
+            String sql = "CREATE TABLE IF NOT EXISTS User (ID INT AUTO_INCREMENT PRIMARY KEY, Name TEXT, LastName TEXT, Age TINYINT)";
+            Query query = session.createSQLQuery(sql).addEntity(User.class);
+            query.executeUpdate();
+        }
     }
 
     @Override
     public void dropUsersTable() {
-        Session session = Util.getSessionFactory().openSession();
-        Transaction transaction = session.beginTransaction();
-        String sql = "DROP TABLE IF EXISTS User";
-        Query query = session.createSQLQuery(sql);
-        query.executeUpdate();
-        session.close();
+        try (Session session = Util.getSessionFactory().openSession();) {
+            Transaction transaction = session.beginTransaction();
+            String sql = "DROP TABLE IF EXISTS User";
+            Query query = session.createSQLQuery(sql);
+            query.executeUpdate();
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-        Session session = Util.getSessionFactory().openSession();
-        Transaction transaction = session.beginTransaction();
-        session.save(new User(name, lastName, age));
-        session.close();
+        try (Session session = Util.getSessionFactory().openSession();) {
+            Transaction transaction = session.beginTransaction();
+            session.save(new User(name, lastName, age));
+        }
     }
 
     @Override
     public void removeUserById(long id) {
-        Session session = Util.getSessionFactory().openSession();
-        Transaction transaction = session.beginTransaction();
-        User user = session.get(User.class, id);
-        session.remove(user);
-        session.close();
+        try (Session session = Util.getSessionFactory().openSession();) {
+            Transaction transaction = session.beginTransaction();
+            User user = session.get(User.class, id);
+            session.remove(user);
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        Session session = Util.getSessionFactory().openSession();
-        Transaction transaction = session.beginTransaction();
-        List<User> userList = session.createQuery("from User").getResultList();
-        session.close();
-        return userList;
+        try (Session session = Util.getSessionFactory().openSession();) {
+            Transaction transaction = session.beginTransaction();
+            List<User> userList = session.createQuery("from User").getResultList();
+            return userList;
+        }
     }
 
     @Override
     public void cleanUsersTable() {
-        Session session = Util.getSessionFactory().openSession();
-        Transaction transaction = session.beginTransaction();
-        String hql = "DELETE FROM User";
-        Query query = session.createQuery(hql);
-        query.executeUpdate();
-        session.close();
+        try (Session session = Util.getSessionFactory().openSession();) {
+            Transaction transaction = session.beginTransaction();
+            String hql = "DELETE FROM User";
+            Query query = session.createQuery(hql);
+            query.executeUpdate();
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -19,41 +19,33 @@ public class Util {
     private static Connection connection;
     private static SessionFactory sessionFactory;
 
-    public static Connection getConnect() {
-        try {
-            return DriverManager.getConnection(url, user, pass);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+    public static Connection getConnect() throws SQLException {
+        return DriverManager.getConnection(url, user, pass);
     }
 
     public static SessionFactory getSessionFactory() {
         if (sessionFactory == null) {
-            try {
-                Configuration configuration = new Configuration();
+            Configuration configuration = new Configuration();
 
-                Properties settings = new Properties();
-                settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
-                settings.put(Environment.URL, url);
-                settings.put(Environment.USER, user);
-                settings.put(Environment.PASS, pass);
+            Properties settings = new Properties();
+            settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+            settings.put(Environment.URL, url);
+            settings.put(Environment.USER, user);
+            settings.put(Environment.PASS, pass);
 
-                settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
-                settings.put(Environment.SHOW_SQL, "true");
-                settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
-                settings.put(Environment.HBM2DDL_AUTO, "create-drop");
+            settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+            settings.put(Environment.SHOW_SQL, "true");
+            settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+            settings.put(Environment.HBM2DDL_AUTO, "create-drop");
 
-                configuration.setProperties(settings);
+            configuration.setProperties(settings);
 
-                configuration.addAnnotatedClass(User.class);
+            configuration.addAnnotatedClass(User.class);
 
-                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
-                        .applySettings(configuration.getProperties()).build();
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                    .applySettings(configuration.getProperties()).build();
 
-                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
         }
         return sessionFactory;
     }
